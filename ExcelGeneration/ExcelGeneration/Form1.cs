80400fe6348a4886667c582d346a83629280acdd
György Dezsényi <gyorgy.dezsenyi@stud.uni-corvinus.hu>
2022-10-07 10:15:04 +0200

    Excel library added

--- ExcelGeneration/ExcelGeneration/Form1.cs
+++ ExcelGeneration/ExcelGeneration/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelGeneration
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+
+        List<Flat> flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            flats = context.Flat.ToList();
         }
     }
 }
