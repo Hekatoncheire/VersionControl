74ef6851d1fc4a517eba9b124e3ecc8859821cc6
György Dezsényi <gyorgy.dezsenyi@stud.uni-corvinus.hu>
2022-10-21 10:36:28 +0200

    chart created

--- SOAP_request/SOAP_request/Form1.cs
+++ SOAP_request/SOAP_request/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace SOAP_request
@@ -28,7 +29,21 @@ namespace SOAP_request
 
         private void CreateChart()
         {
-            
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private void CreateXML(string result)
