f6c0192237d28eeb36b27159b3ca1fbefdd2e70d
György Dezsényi <gyorgy.dezsenyi@stud.uni-corvinus.hu>
2022-10-21 10:31:45 +0200

    Xml created

--- SOAP_request/SOAP_request/Form1.cs
+++ SOAP_request/SOAP_request/Form1.cs
@@ -1,4 +1,5 @@
-﻿using SOAP_request.MnbServiceReference;
+﻿using SOAP_request.Entities;
+using SOAP_request.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,15 +9,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace SOAP_request
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        string XMLResult;
         public Form1()
         {
             InitializeComponent();
             MnbSoapRequest();
+            dataGridView1.DataSource = Rates;
+            CreateXML(XMLResult);
+            CreateChart();
+        }
+
+        private void CreateChart()
+        {
+            
+        }
+
+        private void CreateXML(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
 
         private void MnbSoapRequest()
@@ -33,7 +67,8 @@ namespace SOAP_request
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
 
-
+            XMLResult = result;
         }
+
     }
 }
