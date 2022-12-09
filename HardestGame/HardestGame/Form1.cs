b26088d51c536b1b8be2219d995f63a92a3a9ae7
György Dezsényi <gyorgy.dezsenyi@stud.uni-corvinus.hu>
2022-12-09 10:12:22 +0100

    starting population added, and program tried

--- HardestGame/HardestGame/Form1.cs
+++ HardestGame/HardestGame/Form1.cs
@@ -15,6 +15,11 @@ namespace HardestGame
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +27,10 @@ namespace HardestGame
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
             gc.Start(true);
         }
     }
